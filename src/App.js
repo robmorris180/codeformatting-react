b7621a0e5dab9e393b8c49a77249b92b642efa68
Rob <rob.morris@gocompare.com>
2021-03-16 11:03:38 +0000

    fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import HelloWorld from "./HelloWorld";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './HelloWorld';
+
+// const foo = 'lol';
 
 const App = () => {
-return (
-  <div>
-    <HelloWorld />
+  return (
+    <div>
+      <HelloWorld />
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 export const HelloWord = () => {
-return (
+  return (
     <div>
-        <h1>Hello World!!!</h1>
-  </div>
-)
-}
+      <h1>Hello World!!!</h1>
+    </div>
+  );
+};
